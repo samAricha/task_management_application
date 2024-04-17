bd43022a46f3c73ec4ce8eb01f156728bed60081
aricha_samson <samaricha001@gmail.com>
2024-04-17 13:03:27 +0300

    adding Task model, controller, migration, factory and seeder

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    function tasks(){
+
+
+    }
+
+    function createTaks(){
+
+    }
+
+    function updateTask(){
+
+    }
+
+    function deleteTask(){
+        
+    }
+}
