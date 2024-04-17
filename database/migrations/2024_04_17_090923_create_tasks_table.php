0535c99ec6cfadd5c6431f915ec583e7e1cbcdec
aricha_samson <samaricha001@gmail.com>
2024-04-17 13:16:34 +0300

    setup task model and migration columns

--- database/migrations/2024_04_17_090923_create_tasks_table.php
+++ database/migrations/2024_04_17_090923_create_tasks_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->string('task_name');
+            $table->string('priority');
+            $table->string('status');
             $table->timestamps();
         });
     }
